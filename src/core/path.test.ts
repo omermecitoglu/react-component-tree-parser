266c492d19f68c24d546ec60eab48e6bb081c92a
Omer Mecitoglu <omer.mecitoglu@gmail.com>
2024-10-02 09:20:27 +0700

    chore: replace mocha with jest

--- src/core/path.test.ts
+++ src/core/path.test.ts
@@ -1,31 +1,31 @@
+import {describe,it,expect} from "@jest/globals"
 import path from "node:path";
-import { expect } from "chai";
 import { resolvePath } from "./path";
 
 describe("resolvePath function", () => {
   const rootComponentPath = path.resolve(__dirname, "../test/root.tsx");
 
   it("should determine correctly if the path is external", () => {
-    expect(resolvePath("react", rootComponentPath).isExternal).to.be.true;
-    expect(resolvePath("./module1", rootComponentPath).isExternal).to.be.false;
-    expect(resolvePath("../test/module1", rootComponentPath).isExternal).to.be.false;
-    expect(resolvePath("~/test/module1", rootComponentPath).isExternal).to.be.false;
+    expect(resolvePath("react", rootComponentPath).isExternal).toBe(true);
+    expect(resolvePath("./module1", rootComponentPath).isExternal).toBe(false);
+    expect(resolvePath("../test/module1", rootComponentPath).isExternal).toBe(false);
+    expect(resolvePath("~/test/module1", rootComponentPath).isExternal).toBe(false);
   });
 
   it("should determine correctly if the path is relative", () => {
-    expect(resolvePath("react", rootComponentPath).isRelative).to.be.false;
-    expect(resolvePath("./module1", rootComponentPath).isRelative).to.be.true;
-    expect(resolvePath("../test/module1", rootComponentPath).isRelative).to.be.true;
-    expect(resolvePath("~/test/module1", rootComponentPath).isRelative).to.be.false;
+    expect(resolvePath("react", rootComponentPath).isRelative).toBe(false);
+    expect(resolvePath("./module1", rootComponentPath).isRelative).toBe(true);
+    expect(resolvePath("../test/module1", rootComponentPath).isRelative).toBe(true);
+    expect(resolvePath("~/test/module1", rootComponentPath).isRelative).toBe(false);
   });
 
   it("should resolve absolute paths correctly", () => {
-    expect(resolvePath("react", rootComponentPath).absolute).to.equal("react");
+    expect(resolvePath("react", rootComponentPath).absolute).toBe("react");
     const modulePath1 = resolvePath("./module1", rootComponentPath).absolute;
-    expect(modulePath1).to.equal(path.resolve(__dirname, "../test/module1.tsx"));
+    expect(modulePath1).toBe(path.resolve(__dirname, "../test/module1.tsx"));
     const modulePath2 = resolvePath("~/test/module2", rootComponentPath).absolute;
-    expect(modulePath2).to.equal(path.resolve(__dirname, "../test/module2/index.tsx"));
+    expect(modulePath2).toBe(path.resolve(__dirname, "../test/module2/index.tsx"));
     const modulePath3 = resolvePath("../test/module1", rootComponentPath).absolute;
-    expect(modulePath3).to.equal(path.resolve(__dirname, "../test/module1.tsx"));
+    expect(modulePath3).toBe(path.resolve(__dirname, "../test/module1.tsx"));
   });
 });
---
+++ src/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "@jest/globals";
+import parseComponentTree from "./index";
+
+describe("parseComponentTree", () => {
+  it("should .", () => {
+    expect(parseComponentTree).not.toBeNull()
+  });
+});
